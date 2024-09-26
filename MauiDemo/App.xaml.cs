75bcbbaf306d9964a2cea954cdb3cfea3f946404
pacheco4480 <david9pachecoo@gmail.com>
2024-09-26 19:07:33 +0100

    Layouts - Grid

--- MauiDemo/App.xaml.cs
+++ MauiDemo/App.xaml.cs
@@ -13,7 +13,7 @@
             navPage.BarBackground = Colors.Yellow;
             navPage.BarTextColor = Colors.Blue;
 
-            MainPage = new StackLayoutDemo();
+            MainPage = new GridLayoutDemo();
         }
     }
 }
