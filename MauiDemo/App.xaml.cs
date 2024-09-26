6cc535dafde74acad3c14323230871aa3c813d1d
pacheco4480 <david9pachecoo@gmail.com>
2024-09-26 19:36:19 +0100

    Layouts - FlexLayout

--- MauiDemo/App.xaml.cs
+++ MauiDemo/App.xaml.cs
@@ -13,7 +13,7 @@
             navPage.BarBackground = Colors.Yellow;
             navPage.BarTextColor = Colors.Blue;
 
-            MainPage = new GridLayoutDemo();
+            MainPage = new FlexLayoutDemo();
         }
     }
 }
